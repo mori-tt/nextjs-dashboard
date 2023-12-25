6bf7cd8cd0f200c15f1ad8cae9bd9cc2226a6bf9
mori-tt <morit0301@gmail.com>
2023-12-25 22:33:53 +0900

    🚧 エラーは出ないがbuildできているのか。。。

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
