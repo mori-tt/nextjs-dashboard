8420c9128740453aa4c22046f2f65ca0c961c0c5
mori-tt <morit0301@gmail.com>
2024-02-29 14:02:49 +0900

    🚀 完成

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,19 +8,19 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-const handleSearch = useDebouncedCallback((term) => {
-  console.log(`Searching... ${term}`);
-  const params = new URLSearchParams(searchParams);
-  console.log(term);
-      params.set('page', '1');
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    console.log(term);
+    params.set('page', '1');
 
-  if (term) {
-    params.set('query', term);
-  } else {
-    params.delete('query');
-  }
-  replace(`${pathname}?${params.toString()}`);
-}, 300);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
