8e736732161ca0eb19d4bbf3cf98946f0dfa2069
T_Mori <morit0301@gmail.com>
2024-04-24 21:15:18 +0900

    🚀 完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 
